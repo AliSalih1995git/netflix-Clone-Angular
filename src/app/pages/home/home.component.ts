333b76d94c7bca51b76fda0ccdffc18114040ae3
AliSalih1995git <kvaliswalihtnl@gmail.com>
2023-07-22 15:46:00 +0530

    initial commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  constructor(private service: MovieApiServiceService) {}
+
+  bannerResult: any = [];
+  trendingMovieResult: any = [];
+  actionMovieResult: any = [];
+  adventureMovieResult: any = [];
+  animationMovieResult: any = [];
+  comedyMovieResult: any = [];
+
+  ngOnInit(): void {
+    this.bannerData();
+    this.trendingData();
+    this.actionMovie();
+    this.adventureMovie();
+    this.comedyMovie();
+    this.animationMovie();
+  }
+
+  //bannerData
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      this.bannerResult = result.results;
+    });
+  }
+
+  // trendingData
+  trendingData() {
+    this.service.trendingMovieApiData().subscribe((result) => {
+      this.trendingMovieResult = result.results;
+    });
+  }
+
+  // action
+  actionMovie() {
+    this.service.fetchActionMovies().subscribe((result) => {
+      this.actionMovieResult = result.results;
+    });
+  }
+
+  // adventure
+  adventureMovie() {
+    this.service.fetchAdventureMovies().subscribe((result) => {
+      this.adventureMovieResult = result.results;
+    });
+  }
+
+  // animation
+  animationMovie() {
+    this.service.fetchAnimationMovies().subscribe((result) => {
+      this.animationMovieResult = result.results;
+    });
+  }
+
+  // comedy
+  comedyMovie() {
+    this.service.fetchComedyMovies().subscribe((result) => {
+      this.comedyMovieResult = result.results;
+    });
+  }
+}
